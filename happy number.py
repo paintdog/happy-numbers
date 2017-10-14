ddd922345c8b365efe7306b55c2e9effb6e4e7fc
paintdog <paintdog@users.noreply.github.com>
2017-10-14 21:56:54 +0200

    Update happy number.py

--- happy number.py
+++ happy number.py
@@ -1,7 +1,6 @@
 # Happy numbers
 # https://oeis.org/A007770
 
-
 def check_happy_number(n):
     while True:
         result = sum([int(a) ** 2 for a in str(n)])
@@ -12,9 +11,6 @@ def check_happy_number(n):
         else:
             n = result
 
-
 for i in range(1,1000 + 1):
     if check_happy_number(i):
         print(i, end=", ")
-    else:
-        pass
